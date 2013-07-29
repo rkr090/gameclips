67bc176cd9f83da644c4e8cfc48293092e6dee83
Rohit Kumar <rohitz1990@gmail.com>
2013-07-29 12:56:13 +0530

    content-type validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ActiveRecord::Base
   belongs_to :user, :dependent => :destroy
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_with AttachmentContentTypeValidator, :attributes => :avatar, :content_type => /image/
 end
--- app/models/screen.rb
+++ app/models/screen.rb
@@ -1,9 +1,10 @@
 class Screen < ActiveRecord::Base
   belongs_to :user, :dependent => :destroy
-  validates :user, :presence => true
 
-  validates :image, :attachment_presence => true
-  validates_with AttachmentPresenceValidator, :attributes => :image
+  validates :user, presence: true
+  validates :name, presence: true, length: {maximum: 30}
 
   has_attached_file :image
+  validates_with AttachmentPresenceValidator, :attributes => :image
+  validates_with AttachmentContentTypeValidator, :attributes => :image, :content_type => /image/
 end
