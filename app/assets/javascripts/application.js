ad028a0e64740fe253c0fdc6ab96b940631cd415
Rohit Kumar <rohitz1990@gmail.com>
2013-07-29 17:42:22 +0530

    image carousel homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require jquery.cycle2
 //= require turbolinks
 //= require_tree .
--- app/helpers/screens_helper.rb
+++ app/helpers/screens_helper.rb
@@ -1,2 +1,6 @@
 module ScreensHelper
+
+  def latest_screens
+    Screen.last(5).reverse
+  end
 end
