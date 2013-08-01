a0ca5aeab2584c187ae9497485e2eb6977a8f469
Rohit Kumar <rohitz1990@gmail.com>
2013-08-01 17:32:16 +0530

    data-no-turbolinks on brand link

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,12 +5,4 @@ $(document).ready(function() {
     $('#frm-collapse').toggle();
     $('#frm-btn').hide();
   });
-
-  $('#new-scr-form').hide();
-
-  	$('#new-scr-button').click(function(){
-
-  		$('#new-scr-form').toggle();
-
-  	});
 });
