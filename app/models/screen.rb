93832127b7b71a55b869713728dbfc2a2aa7e326
Rohit Kumar <rohitz1990@gmail.com>
2013-08-03 11:54:48 +0530

    crop screens 550x350-midlarge

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -4,7 +4,7 @@ class Screen < ActiveRecord::Base
   validates :user, presence: true
   validates :name, presence: true, length: {maximum: 30}
 
-  has_attached_file :image
+  has_attached_file :image, :styles => { :midlarge => "550x350>" }
   validates_with AttachmentPresenceValidator, :attributes => :image
   validates_with AttachmentContentTypeValidator, :attributes => :image, :content_type => /image/
   validates_with AttachmentSizeValidator, :attributes => :image, :in => 0..3.megabytes,
