99243cbbdc9ec9e3f5514a8b532a6aaecaccd34b
Rohit Kumar <rohitz1990@gmail.com>
2013-08-03 16:52:58 +0530

    update custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-  $('#frm-collapse').hide();
+  $('#sign_up_form').hide();
+  $('#screen_up_form').hide();
 
-  $('#frm-btn').click(function() {
-    $('#frm-collapse').toggle();
-    $('#frm-btn').hide();
+  $('#sign_up_btn').click(function() {
+    $('#sign_up_form').toggle();
+    $('#sign_up_btn').hide();
+  });
+
+  $('#screen_up_btn').click(function() {
+    $('#screen_up_form').toggle();
+    $('#screen_up_btn').hide();
   });
 });
